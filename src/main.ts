9ec2af70b939339f996fa2bd0a25274df558d898
Masaru Sakasegawa <sakasegawa.masaru@gmail.com>
2023-04-14 17:52:34 +0900

    finished for now

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,16 @@ import helmet from 'helmet';
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
-import { AppModule } from './app.module';
+import { CustomLoggerService } from 'src/common/logger/custom-logger.service';
+import { AppModule } from 'src/app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    bufferLogs: true,
+  });
   const configService = app.get(ConfigService);
   const port = configService.get('PORT') || 3000;
+  app.useLogger(app.get(CustomLoggerService));
   app.use(helmet());
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(port);
