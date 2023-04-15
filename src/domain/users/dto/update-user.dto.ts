74b3ac82ffba5fea8432996c47532de16d67a2e4
Masaru Sakasegawa <sakasegawa.masaru@gmail.com>
2023-04-15 09:00:53 +0900

    finished for now

--- src/domain/users/dto/update-user.dto.ts
+++ src/domain/users/dto/update-user.dto.ts
@@ -1,12 +1,25 @@
 import { IsNotEmpty, MaxLength } from 'class-validator';
 import { PartialType } from '@nestjs/mapped-types';
+import { ApiProperty } from '@nestjs/swagger';
 import { CreateUserDto } from './create-user.dto';
 
 export class UpdateUserDto extends PartialType(CreateUserDto) {
+  @ApiProperty({
+    description: 'user name',
+    type: String,
+    required: true,
+    maxLength: 100,
+  })
   @MaxLength(100)
   @IsNotEmpty()
   name: string;
 
+  @ApiProperty({
+    description: 'password',
+    type: String,
+    required: true,
+    maxLength: 100,
+  })
   @MaxLength(100)
   @IsNotEmpty()
   password: string;
